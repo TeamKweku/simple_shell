0c0bd94a8ef535216eec2ac52241ffb6164c7b29
teamkweku <teamkweku@outlook.com>
2023-05-24 15:20:58 +0000

    Udpate to execution commands

--- exec_handlers.c
+++ exec_handlers.c
@@ -7,13 +7,14 @@
   *
   * Return: nothing
   */
+
 void execute_cmds(char **argv)
 {
 	pid_t pid;
 	char *path;
 	int child_status, exec_status;
 
-	path = path_finder(argv);
+	path = find_path(argv);
 	if (!path)
 		perror("./hsh");
 	else
@@ -26,7 +27,7 @@ void execute_cmds(char **argv)
 			exec_status = execve(path, argv, environ);
 			if (exec_status == -1)
 			{
-				frees1(argv);
+				free_args(argv);
 				exit(EXIT_FAILURE);
 			}
 
@@ -37,7 +38,7 @@ void execute_cmds(char **argv)
 		if (!path)
 			free(path);
 	}
-	frees1(argv);
+	free_args(argv);
 }
 
 /**
@@ -52,25 +53,26 @@ int handle_builtin_cmds(char **argv, char **env)
 {
 	if (_strcmp(argv[0], "setenv") == 0)
 	{
-		set_env(argv, env);
-		 frees1(argv);
+		set_env_var(argv, env);
+		 free_args(argv);
 	}
 	else if (_strcmp(argv[0], "unsetenv") == 0)
 	{
-		unset_env(argv, env);
-		 frees1(argv);
+		unset_env_var(argv, env);
+		 free_args(argv);
 	}
 	else if (_strcmp(argv[0], "cd") == 0)
 	{
 		cd(argv, env);
-		frees1(argv);
+		free_args(argv);
 	}
 	else if (_strcmp(argv[0], "env") == 0)
 	{
 		print_env();
-		frees1(argv);
+		free_args(argv);
 	}
 	else
 		execute_cmds(argv);
 	return (1);
 }
+
